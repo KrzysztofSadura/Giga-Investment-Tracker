189662a857402d610827a87bb8ad2adc8d596809
Krzysztof Sadura <sadura.krzysiek@gmail.com>
2022-12-03 18:30:06 +0100

    added theme colors

--- giga-investment-tracker/projects/giga-investment-tracker/src/app/app.component.ts
+++ giga-investment-tracker/projects/giga-investment-tracker/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    title = 'giga-investment-tracker';
+	title = 'giga-investment-tracker';
 }
