9419d4a8ce6fd9cc9bbb2814a7a09f693f4a2847
Krzysztof Sadura <sadura.krzysiek@gmail.com>
2022-12-04 14:21:38 +0100

    converted app to standalone components

--- giga-investment-tracker/projects/giga-investment-tracker/src/app/app.component.ts
+++ giga-investment-tracker/projects/giga-investment-tracker/src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
+	standalone: true,
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
+	imports: [DashboardComponent],
 })
 export class AppComponent {
 	title = 'giga-investment-tracker';
