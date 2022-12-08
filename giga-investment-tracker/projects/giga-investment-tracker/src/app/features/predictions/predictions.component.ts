d37f7d421f48f7c55f55042734ccf8fad563435d
Krzysztof Sadura <sadura.krzysiek@gmail.com>
2022-12-08 21:04:51 +0100

    added main features components

---
+++ giga-investment-tracker/projects/giga-investment-tracker/src/app/features/predictions/predictions.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-predictions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './predictions.component.html',
+  styleUrls: ['./predictions.component.scss']
+})
+export class PredictionsComponent {
+
+}
