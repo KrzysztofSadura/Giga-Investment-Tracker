50ee1bc0cc890e4adf70b73209f1c4a648315761
Krzysztof Sadura <sadura.krzysiek@gmail.com>
2022-12-13 21:18:46 +0100

    disable debug tracing, delete favicon

--- giga-investment-tracker/projects/giga-investment-tracker/src/main.ts
+++ giga-investment-tracker/projects/giga-investment-tracker/src/main.ts
@@ -2,17 +2,11 @@ import { APP_ROUTES } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import {
-	NoPreloading,
-	PreloadAllModules,
-	provideRouter,
-	withDebugTracing,
-	withPreloading,
-} from '@angular/router';
+import { NoPreloading, provideRouter, withPreloading } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
 	providers: [
-		provideRouter(APP_ROUTES, withPreloading(NoPreloading), withDebugTracing()),
+		provideRouter(APP_ROUTES, withPreloading(NoPreloading)),
 		provideAnimations(),
 	],
 });
