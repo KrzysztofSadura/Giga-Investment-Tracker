ccf21ca5752da91ad401e91aac46f2739cb15f3f
Krzysztof Sadura <sadura.krzysiek@gmail.com>
2023-01-09 21:16:49 +0100

    added local currency to display pipes

--- giga-investment-tracker/projects/giga-investment-tracker/src/app/app.component.ts
+++ giga-investment-tracker/projects/giga-investment-tracker/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import * as PortfolioActions from './data/store/actions';
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
 
 @Component({
 	selector: 'app-root',
@@ -9,11 +11,19 @@ import * as PortfolioActions from './data/store/actions';
 	template: `<app-main-layout></app-main-layout>`,
 	styleUrls: ['./app.component.scss'],
 	imports: [MainLayoutComponent],
+	providers: [
+		{
+			provide: LOCALE_ID,
+			useValue: 'pl-PL',
+		},
+		{ provide: DEFAULT_CURRENCY_CODE, useValue: 'zł' },
+	],
 })
 export class AppComponent implements OnInit {
 	private store = inject(Store);
 
 	ngOnInit(): void {
+		registerLocaleData(localePl);
 		this.store.dispatch(PortfolioActions.getTransactions());
 	}
 }
